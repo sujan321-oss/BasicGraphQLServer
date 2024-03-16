9e1a2a5f1c705281f7bc3805da937bc4ee183fac
sujan321-oss <khumapokharel2076@gmail.com>
2024-03-16 22:03:49 +0530

    All the files are changes

---
+++ build/index.js
@@ -0,0 +1,43 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const server_1 = require("@apollo/server");
+const express4_1 = require("@apollo/server/express4");
+const PORT = 8001;
+const init = () => __awaiter(void 0, void 0, void 0, function* () {
+    const app = (0, express_1.default)();
+    const gqlserver = new server_1.ApolloServer({
+        typeDefs: `
+        type userdata{
+            name:String!
+            houseno:String!
+        }
+
+        type Query{
+            getData:[userdata]
+        }
+        `,
+        resolvers: {
+            Query: {
+                getData: () => [{ name: "khuma pokharel", houseno: 4001 }]
+            }
+        }
+    });
+    yield gqlserver.start();
+    app.use(express_1.default.json());
+    app.use("/graphql", (0, express4_1.expressMiddleware)(gqlserver));
+    app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+});
+init();
--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,40 @@
-import { ApolloServer } from '@apollo/server';
+
 import express from 'express'
-import { expressMiddleware } from '@apollo/server/express4'
+import { ApolloServer } from '@apollo/server'
+import { expressMiddleware } from '@apollo/server/express4';
 
+const PORT=8001
+const init=async ()=>{
+    
+    const app=express()
 
-
-const app = express()
-const PORT = Number(process.env.PORT) || 8001;
-
-async function startServer() {
-    const gqlserver = new ApolloServer(
-        {
-            typeDefs: `
-          type UserData{
-            id:ID!,
+    const gqlserver=new ApolloServer({
+        typeDefs:`
+        type userdata{
             name:String!
-          }
-          type Query{
-            getUserData:[UserData]
-            say(name:String):String
-          }
-        `,
-            resolvers: {
-                Query: {
-                    getUserData: () => [{ id: 12, name: "khuma Pokharel" }],
-                    say: (_, { name }: { name: String }) => "khuma"
+            houseno:String!
+        }
 
-                }
+        type Query{
+            getData:[userdata]
+        }
+        `,
+        
+        resolvers:{
+            Query:{
+                getData: ()=>[{name:"khuma pokharel",houseno:4001}]
             }
         }
-    )
+    })
+    await gqlserver.start()
+    
 
-    await gqlserver.start();
+
     app.use(express.json())
-    app.use("/graphql", expressMiddleware(gqlserver)); // Call the imported expressMidlleware function
+    app.use("/graphql",expressMiddleware(gqlserver))
+    app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-    app.listen(PORT, () => console.log("listening to the port no " + PORT));
 
 }
 
-startServer()
-
+init()
